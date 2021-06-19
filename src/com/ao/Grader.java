79f87cc42f39f5998c27f37d9ee444916f888e63
aidenao <aidenhyao@gmail.com>
2021-06-19 14:17:10 -0700

    init

---
+++ src/com/ao/Grader.java
@@ -0,0 +1,32 @@
+package com.ao;
+
+import com.ao.pobj.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Grader {
+
+    public Student getHighestScoreStudent(List<Student> students) {
+        return null;
+    }
+
+    public Student getLowestScoreStudent(List<Student> students) {
+        return null;
+    }
+
+    public int getAverageScore(List<Student> students) {
+        return -1;
+    }
+
+    public List<Student> getFailedStudents(List<Student> students) {
+        var failedStudents = new ArrayList<Student>();
+        return failedStudents;
+    }
+
+    public List<Student> getFailedStudents(List<Student> students, Predicate<Student> test) {
+        return students.stream().filter(test).collect(Collectors.toList());
+    }
+}
---
+++ src/com/ao/pobj/Student.java
@@ -0,0 +1,7 @@
+package com.ao.pobj;
+
+public class Student {
+    public int id;
+    public String name;
+    public int score;
+}
